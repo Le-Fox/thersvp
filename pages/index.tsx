8e73967bb8b786301bf604eb8e596006c7ad0cbe
Le-Fox <smkupa@gmail.com>
2023-05-09 23:33:51 +0200

    Updated Background Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import NavBarComponent from '@/components/NavBar'
 import RsvpFormComponent from '@/components/RsvpForm'
-import Button from '@/components/button'
+import { useState } from "react"
+import Button from '@/components/Button'
 import Hero from '@/components/Hero'
 
 export default function Home() {
+  const [navbar, setnavbar] = useState(false);
   return (
    <div className='bg-creamy-brown'>
       <Head>
@@ -14,96 +16,99 @@ export default function Home() {
         <link rel="icon" href="/hand-in-hand.png" />
       </Head>
 
-      <NavBarComponent/> 
-      <section className=" text-kinda-black bg-no-repeat bg-bottom bg-auto  bg-[url('../public/d1dbd467-a269-6c23-8751-fa5755702c36_620.jpg')] min-h-screen ">
-        <Hero/>
-        <div className='lg:px-44'>
-          <div className='text-left md:text-lg text-xl font-Mathanifo'>
-            <p className='pl-4 m-4'>26.09.14</p>
-          </div>
-          <Button/>
-        </div>
-        
-        
-      </section>
+      <main className='snap-y snap-mandatory transition overflow-hidden'>
+        {/* <NavBarComponent/>  */}
+        <section className=" text-kinda-black bg-repeat-x  bg-top bg-auto  bg-[url('../public/TightBackgroundImage.jpg')] lg:min-h-screen  snap-start z-20 flex  flex-col justify-center items-center">
 
-      <div className='lg:px-44'>
-          
-        <section  id="ourStory">
-          <div className='text-center font-Mathanifo text-6xl mb-4'>
-            <h2>
-            Our Story
-            </h2>
-          </div>
-          
-          <div className='leading-6 text-center font-AddingtonT text-2xl'>
-            <p>
-            Busi and Josh met back in October 2018 after being set up on a blind date through mutual friends. Busi was struck by Josh’s kindness, while Josh instantly fell for Busi’s dark sense of humour.
-            </p>
+            <Hero/>
 
-            <br />
-
-            <p>
-            They couldn’t keep away from each other in the following weeks. Before they knew it, weeks rolled into months, then months rolled into years. In June last year, Josh got down on one knee… Busi said yes!
-            </p>
-
-            <br />
-
-          </div>
-          
-        </section>
-
-        <section  id="schedule">
-
-          <div className='text-center font-Mathanifo text-6xl mb-4'>
-            <h2>
-              Schedule
-            </h2>
-          </div>
-          
-        </section>
-
-
-        <section  id="GiftRegistry">
-
-          <div className='text-center font-Mathanifo text-6xl mb-4'>
-            <h2>Our Gift Registry</h2>
-          </div>
-          
-          <div className='leading-normal font-AddingtonT text-2xl'>
-            <p>
-            Should you wish to honour us with a gift, we hope that this registry will make it more convenient for you to select something that you know we will love.
-            </p>
-          </div>
-          
-        </section>
-
-        <section  id="RSVP">
-          <div className='text-center font-Mathanifo text-5xl mb-4'>
-            <h2>RSVP</h2>
-          </div>
-          
-          <div className='leading-normal font-AddingtonT text-2xl'>
-            <p>
-            The favour of your response is requested as soon as you can.
-            </p>
-          </div>
-
-          <div className='flex justify-center'>
+          <div className='lg:px-44 flex  flex-col justify-start items-start'>
+            <div className='text-left md:text-lg text-xl font-Mathanifo'>
+              <p className='lg:pl-4 lg:m-4 mb-2'>26.09.14</p>
+            </div>
             <Button/>
           </div>
-
+          
           
         </section>
+        <div className='lg:px-44 '>
+            
+          <section  id="ourStory" className='snap-center  relative min-h-1/2   border-b-3 border-kinda-black my-5'>
+            <div className='text-center font-Mathanifo lg:text-6xl mb-4 text-4xl'>
+              <h2>
+              Our Story
+              </h2>
+            </div>
+            
+            <div className='leading-6 text-center font-AddingtonT lg:text-2xl text-base'>
+              <p>
+              Busi and Josh met back in October 2018 after being set up on a blind date through mutual friends. Busi was struck by Josh’s kindness, while Josh instantly fell for Busi’s dark sense of humour.
+              </p>
 
-        <section>
-        {/* <RsvpFormComponent/> */}
-        </section>
-        
-      </div>
+              <br />
 
-        
-    
+              <p>
+              They couldn’t keep away from each other in the following weeks. Before they knew it, weeks rolled into months, then months rolled into years. In June last year, Josh got down on one knee… Busi said yes!
+              </p>
+
+              <br />
+
+            </div>
+            
+          </section>
+
+          <section  id="schedule" className='snap-center min-h-1/2  border-b-3 border-kinda-black my-5'>
+
+            <div className='text-center font-Mathanifo lg:text-6xl mb-4 text-4xl'>
+              <h2>
+                Schedule
+              </h2>
+            </div>
+
+          </section>
+
+
+          <section  id="GiftRegistry" className='snap-center min-h-1/2 border-b-3 border-kinda-black my-5'>
+
+            <div className='text-center font-Mathanifo lg:text-6xl mb-4 text-4xl'>
+              <h2>Our Gift Registry</h2>
+            </div>
+            
+            <div className='leading-normal font-AddingtonT lg:text-2xl text-base'>
+              <p>
+              Should you wish to honour us with a gift, we hope that this registry will make it more convenient for you to select something that you know we will love.
+              </p>
+              <br />
+              
+            </div>
+            
+          </section>
+
+          <section id="RSVP" className='snap-center min-h-1/2 border-b-3 '>
+            <div className='text-center font-Mathanifo lg:text-6xl mb-4 text-4xl'>
+              <h2>RSVP</h2>
+            </div>
+            
+            <div className='leading-normal font-AddingtonT lg:text-2xl text-base'>
+              <p>
+              The favour of your response is requested as soon as you can.
+              </p>
+              <br />
+            </div>
+
+            <div className='flex justify-center'>
+              <Button/>
+            </div>
+
+            
+          </section>
+
+          <section>
+          {/* <RsvpFormComponent/> */}
+          </section>
+          
+        </div>
+      </main>
     </div>
   )
 }
